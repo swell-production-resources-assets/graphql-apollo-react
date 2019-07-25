140aebb054ad78d281b57f708c1672d1764b35e9
Abby Chao <abigail.chao@gmail.com>
2019-07-25 19:19:09 -0400

    Added subscriptions within Query component

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -19,6 +19,27 @@ const LinkList = () => {
     }
   `;
 
+  const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+      newLink {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  `;
+
   // This is a sample implementation of a query with apollo-boost (must un-comment import above)
   // const client = new ApolloClient({ uri: 'http://localhost:4000' });
   // client.query({
@@ -27,17 +48,54 @@ const LinkList = () => {
   //   .then(data => console.log('fetched directly with apollo-boost:', data))
   //   .catch(error => console.error(error));
 
+  // 
+
+
+  /*
+  react-apollo gives us special components like Query, Mutation, and Subscription. These components use React's render prop pattern, which allows us to make components more reusable by specifying what the component renders outside of the component definition.
+  
+  Learn more here: https://reactjs.org/docs/render-props.html
+  
+  In this case, Query will run the anonymous function that we provide, instead of running its own render method. The function is automatically passed a single argument which includes:
+  - data: contains the results of the Graph QL query
+  - several Apollo methods: fetchMore, refetch, startPolling, stopPolling, subscribeToMore, updateQuery
+  - other goodies: error, loading, networkStatus, and variables
+  
+  Query also automatically creates a fetch request for the FEED_QUERY that we passed as a prop.
+
+  subscribeToMore tells the Query to re-render its children based on new data coming back from NEW_LINKS_SUBSCRIPTION
+  */
+
   return (
     <Query query={FEED_QUERY}>
-      {({ loading, error, data }) => {
+      {(arg) => {
+        const { loading, error, data, subscribeToMore } = arg;
         if (loading) return <div>Fetching</div>;
         if (error) return <div>Error</div>;
 
-        console.log(data);
-        const linksToRender = data.feed.links;
+        subscribeToMore({
+          document: NEW_LINKS_SUBSCRIPTION,
+          // prev is the list of all the links previously rendered
+          // the second parameter is an object containing "subscriptionData" and "variables"
+          updateQuery: (prev, { subscriptionData }) => {
+            console.log(prev)
+            if (!subscriptionData.data) return prev;
+            const newLink = subscriptionData.data.newLink;
+            // The next two lines prevent re-rendering if we already have the new Link showing
+            // const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+            // if (exists) return prev;
+            return Object.assign({}, { // tutorial had "prev" as 2nd argument, but unnecessary
+              feed: {
+                links: [...prev.feed.links, newLink],
+                count: prev.feed.links.length + 1,
+                __typename: prev.feed.__typename,
+              }
+            });
+          },
+        })
 
         return (
-          linksToRender.map(link => <li><Link key={link.id} link={link} /></li>)
+          data.feed.links.map(link => <li><Link key={link.id} link={link} /></li>)
         )
       }}
     </Query>
--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
 const httpLink = createHttpLink({ uri: 'http://localhost:4000' });
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      // authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink // with authentication: authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache(),
 });
 
