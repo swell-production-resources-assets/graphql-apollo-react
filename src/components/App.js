35e366e1a2ce086967700bb59fbc8418a0fa0b80
Abby Chao <abigail.chao@gmail.com>
2019-07-31 10:56:40 -0400

    Implemented Subscription component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Link from './Link';
+import gql from 'graphql-tag';
+import { Subscription } from 'react-apollo'
+
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+subscription {
+  newLink {
+    id
+    url
+    description
+    createdAt
+    postedBy {
+      id
+      name
+    }
+    votes {
+      id
+      user {
+        id
+      }
+    }
+  }
+}
+`;
 
 const App = () => {
   return (
     <>
+      <Subscription subscription={NEW_LINKS_SUBSCRIPTION}>
+        {(obj) => {
+          const { loading, data } = obj;
+          if (loading) return <p>Waiting for new data</p>;
+          return (
+            <>
+              New link posted: <Link key={data.newLink.id} link={data.newLink} />
+            </>
+          );
+        }}
+      </Subscription>
+      <hr />
       <LinkList />
       <CreateLink />
     </>
